ec32ca487e658738c1b86f4e426f1c23fd7e2d78
Ansh Gautam <creatoransh786@gmail.com>
2026-03-29 22:48:53 +0530

    init: initialize git repository and add project configuration files

--- test_error.py
+++
@@ -1,7 +0,0 @@
-def calculate_risk(value):
-    # This will cause a ZeroDivisionError
-    return value / 0
-
-if __name__ == "__main__":
-    print("Starting risk calculation...")
-    calculate_risk(100)
